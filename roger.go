9e11ff4c84f95a5c5e697ad8ab608b7e5b98bdec
Dings <deen.job@qq.com>
2020-05-04 17:03:52 +0800

    init first commit

---
+++ roger.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 HigKer
+// Open Source: MIT License
+// Author: SDing <deen.job@qq.com>
+// Date: 2020/5/4 - 4:59 下午
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Roger is distribution Key-value cache system.")
+}
